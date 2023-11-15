7167faf73f7ea7551d5d2910c452076b6e9a0a95
igor <igor.kostenko.1999@mail.ru>
2023-11-15 23:03:22 +0300

    refactor

--- src/main/java/com/words/finder/controller/WordFinderController.java
+++ src/main/java/com/words/finder/controller/WordFinderController.java
@@ -5,6 +5,7 @@ import com.words.finder.model.FindDto;
 import com.words.finder.model.UserInputFindDto;
 import com.words.finder.service.WordFilterService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,13 +15,14 @@ import reactor.core.publisher.Mono;
 
 import java.util.List;
 
+@Tag(name = "Word finder", description = "Контроллер для поска слов")
 @RestController
 @RequiredArgsConstructor
 public class WordFinderController {
     private final WordFilterService wordFilterService;
     private final Mapper<FindDto, UserInputFindDto> mapper;
 
-    @Operation(description = "Find word by included and excluded letters")
+    @Operation(description = "Ищет слова по маске, а также по списку включеных, исключенных слов и исключающих масок")
     @PostMapping("/find")
     public ResponseEntity<Mono<List<String>>> findWord(
             @RequestBody UserInputFindDto findRequest
